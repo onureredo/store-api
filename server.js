bc62e5c6660ea15a50191e775855eef22d9d22ef
Onur Eredo <onureredo@yahoo.com>
2023-04-25 21:04:25 +0200

    added test products

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 8000;
 
 const products = require('./utils/products.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send(products);
